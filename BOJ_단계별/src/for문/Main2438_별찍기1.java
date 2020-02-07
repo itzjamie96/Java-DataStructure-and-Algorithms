859d2ce60cc2499f12b69eb2cac65893bc7dd05f
Jamie Lee <itzjamie96@gmail.com>
2020-02-07 23:44:25 +0900

    BOJ for문 2438 별찍기

---
+++ BOJ_단계별/src/for문/Main2438_별찍기1.java
@@ -0,0 +1,20 @@
+package for¹®;
+
+import java.util.Scanner;
+
+public class Main2438_º°Âï±â1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for (int i=1; i<=N; i++) {			
+			for (int j=1; j<=i; j++ ) {
+				System.out.print("*");				
+			}			
+			System.out.println();
+		}
+	}
+}
