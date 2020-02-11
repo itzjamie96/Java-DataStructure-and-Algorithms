79ac630dbf694de16bfa11a54c6752eff324ae37
Jamie Lee <itzjamie96@gmail.com>
2020-02-11 22:07:44 +0900

    [update] 문제 이름 변경

--- BOJ_단계별/src/c_for문/Main11021_A합B_7.java
+++ BOJ_단계별/src/c_for문/Main11021_A합B_7.java
@@ -5,23 +5,28 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main11022_A합B빼기8 {
+public class Main11021_A합B_7 {
 
 	public static void main(String[] args) throws Exception {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int T = Integer.parseInt(br.readLine());
 		
+		
 		for (int i=1; i<=T; i++) {
-			String[] num = br.readLine().split(" ");
 			
+			String[] num = br.readLine().split(" ");
+
 			int A = Integer.parseInt(num[0]);
 			int B = Integer.parseInt(num[1]);
 			
-			int sum = A+B;
+			//int sum = A+B;
 			
-			bw.write("Case #" + i + ": " + A + " + " + B + " = " + sum + "\n");
+			// ****** 그냥 A+B를 쓰면 "AB"가 나옴
+			// ****** (A+B)를 해줘야 A와 B의 숫자합이 나오더라....
+			bw.write("Case #"+ i + ": " + (A+B) + "\n");
 		}
 		bw.flush();
 	}
