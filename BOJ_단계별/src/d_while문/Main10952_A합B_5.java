49c69641b0ee2a51c21fe7174ebf58e774e30693
Jamie Lee <itzjamie96@gmail.com>
2020-02-11 22:08:40 +0900

    BOJ while문 10952 A+B

---
+++ BOJ_단계별/src/d_while문/Main10952_A합B_5.java
@@ -0,0 +1,20 @@
+package d_while¹®;
+
+import java.util.Scanner;
+
+public class Main10952_AÇÕB_5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+				
+		while (true) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+		
+			if (A==0 && B ==0) break;
+			System.out.println(A+B);
+		}
+
+	}
+
+}
