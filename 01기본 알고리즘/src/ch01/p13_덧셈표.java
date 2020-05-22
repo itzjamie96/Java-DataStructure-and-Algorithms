e54e6b466098e23ca2d033cffb0f16c6d8588d0d
Jamie Lee <itzjamie96@gmail.com>
2020-05-22 22:41:49 +0900

    [ADD] 기본 알고리즘: 덧셈표

---
+++ 01기본 알고리즘/src/ch01/p13_덧셈표.java
@@ -0,0 +1,23 @@
+package ch01;
+
+public class p13_덧셈표 {
+
+	public static void main(String[] args) {
+		System.out.print("  |");
+		for(int i=1; i<10; i++) {
+			System.out.printf("%3d",i);			
+		}
+		System.out.println();
+		System.out.println("--+---------------------------");
+		
+		for(int i=1; i<10; i++) {
+				System.out.print(i+" |");
+			for(int j=1; j<10; j++) {
+				System.out.printf("%3d", i+j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
