9e820023fff5bfcff6e5bf738465c93a4ea2e45a
Jamie Lee <itzjamie96@gmail.com>
2020-05-22 22:20:04 +0900

    [ADD] 기본 알고리즘: 두 자릿수 제한

---
+++ 01기본 알고리즘/src/ch01/ex06_자릿수제한.java
@@ -0,0 +1,24 @@
+package ch01;
+
+import java.util.Scanner;
+
+public class ex06_자릿수제한 {
+
+	public static void main(String[] args) {
+		// 2의 자리 양수로만 제한하는 프로그램
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		
+		// n이 10보다 작던가 99보다 크면 n입력하라고 계속 말해주기
+		do {
+			System.out.print("n: ");
+			n = sc.nextInt();
+		} while(n<10 || n>99);
+		
+		System.out.println("두자릿수 n: "+n);
+
+	}
+
+}
